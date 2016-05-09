ff042b38d3ae7c7d44a5c3fbee2bd8a875c20209
Bijaya Gautam <bijaya4738@users.noreply.github.com>
2016-05-09 15:33:25 +0545

    Add files via upload

--- compare.py
+++ compare.py
@@ -1,10 +1,13 @@
 x = input("enter a number x")
-y = input ("enter a number y")
-def compare(a,b):
-    if a > b:
-        print "a is greater than b","& a is",a
-    elif b >a:
-        print "b is greater than a","& b is ",b
+y = input("enter a number y")
+z = input("enter a number z")
+def compare(a,b,c):
+    if (a > b and a > c):
+        print "x is the max no","and max is:",a
+    elif (b > c and b > a):
+        print "y is the max","and max is:",b
+    elif (c > a and c > b):
+        print "z is the max","and max is:",c
     else:
-        print "your entry is not valid"
-compare(x,y)
+        print "your entry is invalid"
+compare (x,y,z)
