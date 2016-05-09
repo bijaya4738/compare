0022b320f3b2e5f7d5319c6832b60186f5baa7b4
Bijaya Gautam <bijaya4738@users.noreply.github.com>
2016-05-09 16:56:41 +0545

    Add files via upload

--- strlen.py
+++ strlen.py
@@ -1,4 +1,11 @@
 s = raw_input("enter string")
-def strlen(a):
-    print len(a)
-strlen(s) 
+def stringlen(a):
+    length = 0
+    for i in a:
+        if i == "":
+            break
+        else:
+            length+=1
+    print length
+    
+stringlen(s) 
