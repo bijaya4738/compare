90e1ad8f88569e9819aaf603d8225485940a3a78
Bijaya Gautam <bijaya4738@users.noreply.github.com>
2016-05-15 17:01:55 +0545

    Add files via upload

---
+++ overlapping.py
@@ -0,0 +1,24 @@
+length = input("enter length of list")
+list_a=[]
+list_b=[]
+for i in range(length):
+    a = raw_input("l1")
+    if(a.isdigit()):
+        list_a.insert(i,float(a))
+    else:
+        list_a.insert(i,a)
+for i in range(length):
+    b = raw_input("l2")
+    if(b.isdigit()):
+        list_b.insert(i,float(b))
+    else:
+        list_b.insert(i,b)
+
+print list_a
+print list_b
+def is_member(x,y):
+    for 
+print is_member(list_a,list_b)
+    
+
+            
