127d4d170f862b02301be06f2e3b9798db3c6591
Bijaya Gautam <bijaya4738@users.noreply.github.com>
2016-05-18 14:58:41 +0545

    Add files via upload

---
+++ maxnumber.py
@@ -0,0 +1,14 @@
+def max_in():
+    y=[]
+    a = input("enter range")
+    for i in range(a):
+        b = input("enter element")
+        y.insert(i,b)
+    for element in y:
+            return max(y)
+print max_in()  
+
+    
+    
+    
+    
