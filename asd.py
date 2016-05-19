5a1271fea46e48dbfb39d27766e6eaad6b6a5c5a
Bijaya Gautam <bijaya4738@users.noreply.github.com>
2016-05-19 12:44:32 +0545

    Add files via upload

--- asd.py
+++ asd.py
@@ -1,9 +1,5 @@
-def max_in(howmany,element):
-    y=[]
-    a=howmany
-    for i in range(a):
-        b=element
-        y.insert(i,b)
+y=[]
+def max_in(y):
     maxno = y[0]
     for num in y:
         if maxno < num:
