ba50e9e6eec6515daca4c1b3d786cbdc9909fda8
Bijaya Gautam <bijaya4738@users.noreply.github.com>
2016-05-22 16:51:52 +0545

    Add files via upload

--- mapingstrlength.py
+++ mapingstrlength.py
@@ -1,13 +1,10 @@
 y=[]
-z=[]
 def word_len(y):
+    z=[]
     for i in range(len(y)):
         a=len(y[i])
         z.insert(i,a)
         if len(z)==len(y):
-            print z
+            return z
             break
-word_len(y)
 
-
-      
