339eb0d3d11f97c5d329126dad366e1e29e34ed6
Bijaya Gautam <bijaya4738@users.noreply.github.com>
2016-05-22 17:32:05 +0545

    Add files via upload

--- mapingstrlengthsum.py
+++ mapingstrlengthsum.py
@@ -1,12 +1,12 @@
-y=[]
+y=["bijay","wrufesh"]
 def word_len(y):
     z=[]
     for i in range(len(y)):
         a=int(len(y[i]))
         z.insert(i,a)
         if len(z)==len(y):
-            sum =0
-            for i in z:
-                sum=sum+i
-            return sum
-            break
+            return z
+sum=0
+for x in word_len(y):
+    sum=sum+x
+print sum
