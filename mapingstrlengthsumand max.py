1026c36a4d990a3a89da77c0d476b50b71fe8927
Bijaya Gautam <bijaya4738@users.noreply.github.com>
2016-05-23 10:19:43 +0545

    Add files via upload

---
+++ mapingstrlengthsumand max.py
@@ -0,0 +1,17 @@
+y=["bijay","wrufesh"]
+def word_len(y):
+    z=[]
+    for i in range(len(y)):
+        a=int(len(y[i]))
+        z.insert(i,a)
+        if len(z)==len(y):
+            return z
+sum=0
+for x in word_len(y):
+    sum=sum+x
+sum
+max=0
+for num in word_len(y):
+    max<num
+    max=num
+max
