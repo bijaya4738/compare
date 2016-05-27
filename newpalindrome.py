3799d7d59db79ea52e3bf46c82b8db26ec34d9f1
Bijaya Gautam <bijaya4738@users.noreply.github.com>
2016-05-27 12:28:30 +0545

    Add files via upload

---
+++ newpalindrome.py
@@ -0,0 +1,12 @@
+a=str()
+def new_palindrome(a):
+    b = a.replace(" ","")
+    pal = ""
+    for i in b:
+       pal = i+pal
+       if pal == b:
+           return"is palindrome"
+       elif pal != b and len(b) == len(pal):
+          return"is not palindrome"
+print new_palindrome(a)
+
