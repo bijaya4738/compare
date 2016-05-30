bb5c08611498a8f1eb37d8f8def2bbf494fb4cde
Bijaya Gautam <bijaya4738@users.noreply.github.com>
2016-05-30 16:30:37 +0545

    Add files via upload

--- 99 bottle of bear.py
+++ 99 bottle of bear.py
@@ -1,6 +1,17 @@
+import string
 import re
-a="99 bottles of beer on the wall"
+a=str("99 bottles of beer on the wall Take one down pass it around ")
 def bottle(a):
-    a = re.findall("[-+]?\d+[\.]?\d*",a)
-    print a
+    d = re.findall(r'\w+',a)
+    f = int(d[0])
+    g = (d[1:7])
+    g = string.join(g,' ')
+    i = (d[1:5])
+    i = string.join(i,' ')
+    j = (d[6:13])
+    j = string.join(j,' ')
+    for counter in range(f,0,-1):
+        print counter,g
+        print counter,i,j
 bottle(a)
+  
